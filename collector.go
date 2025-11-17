9ca817568aebb1f2a4a95ac769ffff44870506c6
Phillip Schichtel <phillip@schich.tel>
2025-11-18 00:53:31 +0100

    Introduce additional metrics

--- collector.go
+++ collector.go
@@ -63,6 +63,26 @@ var (
 		"The attainable upstream bits per second rate",
 		nil, nil,
 	)
+	interleaveDepthDownDesc = prometheus.NewDesc(
+		prometheus.BuildFQName(namespace, "downstream", "interleave_depth"),
+		"The amount of interleaving configured for the downstream",
+		nil, nil,
+	)
+	interleaveDepthUpDesc = prometheus.NewDesc(
+		prometheus.BuildFQName(namespace, "upstream", "interleave_depth"),
+		"The amount of interleaving configured for the upstream",
+		nil, nil,
+	)
+	actualPsdDownDesc = prometheus.NewDesc(
+		prometheus.BuildFQName(namespace, "downstream", "actual_psd_db"),
+		"The actual downstream power spectrum density in dB",
+		nil, nil,
+	)
+	actualPsdUpDesc = prometheus.NewDesc(
+		prometheus.BuildFQName(namespace, "upstream", "actual_psd_db"),
+		"The actual upstream power spectrum density in dB",
+		nil, nil,
+	)
 	snrMarginDownDesc = prometheus.NewDesc(
 		prometheus.BuildFQName(namespace, "downstream", "snr_margin_db"),
 		"The downstream SNR margin in dB",
@@ -71,6 +91,137 @@ var (
 	snrMarginUpDesc = prometheus.NewDesc(
 		prometheus.BuildFQName(namespace, "upstream", "snr_margin_db"),
 		"The downstream SNR margin in dB",
+		nil, nil,
+	)
+
+	bitswapActiveNearEndDesc = prometheus.NewDesc(
+		prometheus.BuildFQName(namespace, "near_end", "bitswap_active"),
+		"Whether bitswap is active on the near end",
+		nil, nil,
+	)
+	bitswapActiveFarEndDesc = prometheus.NewDesc(
+		prometheus.BuildFQName(namespace, "far_end", "bitswap_active"),
+		"Whether bitswap is active on the far end",
+		nil, nil,
+	)
+	reTxActiveNearEndDesc = prometheus.NewDesc(
+		prometheus.BuildFQName(namespace, "near_end", "retx_active"),
+		"Whether retransmission is active on the near end",
+		nil, nil,
+	)
+	reTxActiveFarEndDesc = prometheus.NewDesc(
+		prometheus.BuildFQName(namespace, "far_end", "retx_active"),
+		"Whether retransmission is active on the far end",
+		nil, nil,
+	)
+	attenuationNearEndDesc = prometheus.NewDesc(
+		prometheus.BuildFQName(namespace, "near_end", "attenuation_db"),
+		"The attenuation on the near end in dB",
+		nil, nil,
+	)
+	attenuationFarEndDesc = prometheus.NewDesc(
+		prometheus.BuildFQName(namespace, "far_end", "attenuation_db"),
+		"The attenuation on the far end in dB",
+		nil, nil,
+	)
+	crcCountNearEndDesc = prometheus.NewDesc(
+		prometheus.BuildFQName(namespace, "near_end", "crc_errors_total"),
+		"The number of CRC errors on the near end",
+		nil, nil,
+	)
+	crcCountFarEndDesc = prometheus.NewDesc(
+		prometheus.BuildFQName(namespace, "far_end", "crc_errors_total"),
+		"The number of CRC errors on the far end",
+		nil, nil,
+	)
+	erroredSecondsNearEndDesc = prometheus.NewDesc(
+		prometheus.BuildFQName(namespace, "near_end", "errored_seconds_total"),
+		"The number of seconds the near end was errored",
+		nil, nil,
+	)
+	erroredSecondsFarEndDesc = prometheus.NewDesc(
+		prometheus.BuildFQName(namespace, "far_end", "errored_seconds_total"),
+		"The number of seconds the far end was errored",
+		nil, nil,
+	)
+	severelyErroredSecondsNearEndDesc = prometheus.NewDesc(
+		prometheus.BuildFQName(namespace, "near_end", "severely_errored_seconds_total"),
+		"The number of seconds the near end was severely errored",
+		nil, nil,
+	)
+	severelyErroredSecondsFarEndDesc = prometheus.NewDesc(
+		prometheus.BuildFQName(namespace, "far_end", "severely_errored_seconds_total"),
+		"The number of seconds the far end was severely errored",
+		nil, nil,
+	)
+	unavailableSecondsNearEndDesc = prometheus.NewDesc(
+		prometheus.BuildFQName(namespace, "near_end", "unavailable_seconds_total"),
+		"The number of seconds the near end was unavailable",
+		nil, nil,
+	)
+	unavailableSecondsFarEndDesc = prometheus.NewDesc(
+		prometheus.BuildFQName(namespace, "far_end", "unavailable_seconds_total"),
+		"The number of seconds the far end was unavailable",
+		nil, nil,
+	)
+	hecErrorCountNearEndDesc = prometheus.NewDesc(
+		prometheus.BuildFQName(namespace, "near_end", "hec_errors_total"),
+		"The number of header errors on the near end",
+		nil, nil,
+	)
+	hecErrorCountFarEndDesc = prometheus.NewDesc(
+		prometheus.BuildFQName(namespace, "far_end", "hec_errors_total"),
+		"The number of header errors on the far end",
+		nil, nil,
+	)
+	losFailureCountNearEndDesc = prometheus.NewDesc(
+		prometheus.BuildFQName(namespace, "near_end", "los_failures_total"),
+		"The number of Loss of Signal failures at the near end",
+		nil, nil,
+	)
+	losFailureCountFarEndDesc = prometheus.NewDesc(
+		prometheus.BuildFQName(namespace, "far_end", "los_failures_total"),
+		"The number of Loss of Signal failures at the far end",
+		nil, nil,
+	)
+	lofFailureCountNearEndDesc = prometheus.NewDesc(
+		prometheus.BuildFQName(namespace, "near_end", "lof_failures_total"),
+		"The number of Loss of Frame failures at the near end",
+		nil, nil,
+	)
+	lofFailureCountFarEndDesc = prometheus.NewDesc(
+		prometheus.BuildFQName(namespace, "far_end", "lof_failures_total"),
+		"The number of Loss of Frame failures at the far end",
+		nil, nil,
+	)
+	lprFailureCountNearEndDesc = prometheus.NewDesc(
+		prometheus.BuildFQName(namespace, "near_end", "lpr_failures_total"),
+		"The number of Loss of Power failures at the near end",
+		nil, nil,
+	)
+	lprFailureCountFarEndDesc = prometheus.NewDesc(
+		prometheus.BuildFQName(namespace, "far_end", "lpr_failures_total"),
+		"The number of Loss of Power failures at the far end",
+		nil, nil,
+	)
+	lcdFailureCountNearEndDesc = prometheus.NewDesc(
+		prometheus.BuildFQName(namespace, "near_end", "lcd_failures_total"),
+		"The number of Loss of Cell Delineation failures at the near end",
+		nil, nil,
+	)
+	lcdFailureCountFarEndDesc = prometheus.NewDesc(
+		prometheus.BuildFQName(namespace, "far_end", "lcd_failures_total"),
+		"The number of Loss of Cell Delineation failures at the far end",
+		nil, nil,
+	)
+	rfecCountNearEndDesc = prometheus.NewDesc(
+		prometheus.BuildFQName(namespace, "near_end", "reed_solomon_forward_error_corrections_total"),
+		"The number of Reed–Solomon Forward Error Corrections at the near end",
+		nil, nil,
+	)
+	rfecCountFarEndDesc = prometheus.NewDesc(
+		prometheus.BuildFQName(namespace, "far_end", "reed_solomon_forward_error_corrections_total"),
+		"The number of Reed–Solomon Forward Error Corrections at the far end",
 		nil, nil,
 	)
 )
@@ -84,8 +235,39 @@ func (e *Exporter) Describe(ch chan<- *prometheus.Desc) {
 	ch <- actualRateUpDesc
 	ch <- attainableRateDownDesc
 	ch <- attainableRateUpDesc
+	ch <- interleaveDepthDownDesc
+	ch <- interleaveDepthUpDesc
+	ch <- actualPsdDownDesc
+	ch <- actualPsdUpDesc
 	ch <- snrMarginDownDesc
 	ch <- snrMarginUpDesc
+
+	ch <- bitswapActiveNearEndDesc
+	ch <- bitswapActiveFarEndDesc
+	ch <- reTxActiveNearEndDesc
+	ch <- reTxActiveFarEndDesc
+	ch <- attenuationNearEndDesc
+	ch <- attenuationFarEndDesc
+	ch <- crcCountNearEndDesc
+	ch <- crcCountFarEndDesc
+	ch <- erroredSecondsNearEndDesc
+	ch <- erroredSecondsFarEndDesc
+	ch <- severelyErroredSecondsNearEndDesc
+	ch <- severelyErroredSecondsFarEndDesc
+	ch <- unavailableSecondsNearEndDesc
+	ch <- unavailableSecondsFarEndDesc
+	ch <- hecErrorCountNearEndDesc
+	ch <- hecErrorCountFarEndDesc
+	ch <- losFailureCountNearEndDesc
+	ch <- losFailureCountFarEndDesc
+	ch <- lofFailureCountNearEndDesc
+	ch <- lofFailureCountFarEndDesc
+	ch <- lprFailureCountNearEndDesc
+	ch <- lprFailureCountFarEndDesc
+	ch <- lcdFailureCountNearEndDesc
+	ch <- lcdFailureCountFarEndDesc
+	ch <- rfecCountNearEndDesc
+	ch <- rfecCountFarEndDesc
 }
 
 // Collect fetches the stats from the draytek router and delivers them as
@@ -119,9 +301,107 @@ func (e *Exporter) Collect(ch chan<- prometheus.Metric) {
 		attainableRateUpDesc, prometheus.GaugeValue, float64(status.AttainableRateUpstream),
 	)
 	ch <- prometheus.MustNewConstMetric(
+		interleaveDepthDownDesc, prometheus.GaugeValue, float64(status.InterleaveDepthDownstream),
+	)
+	ch <- prometheus.MustNewConstMetric(
+		interleaveDepthUpDesc, prometheus.GaugeValue, float64(status.InterleaveDepthUpstream),
+	)
+	ch <- prometheus.MustNewConstMetric(
+		actualPsdDownDesc, prometheus.GaugeValue, status.ActualPSDDownstream,
+	)
+	ch <- prometheus.MustNewConstMetric(
+		actualPsdUpDesc, prometheus.GaugeValue, status.ActualPSDUpstream,
+	)
+	ch <- prometheus.MustNewConstMetric(
 		snrMarginDownDesc, prometheus.GaugeValue, status.SNRMarginDownstream,
 	)
 	ch <- prometheus.MustNewConstMetric(
 		snrMarginUpDesc, prometheus.GaugeValue, status.SNRMarginUpstream,
 	)
+
+	ch <- prometheus.MustNewConstMetric(
+		bitswapActiveNearEndDesc, prometheus.GaugeValue, optionToFloat64(status.BitswapNearEnd),
+	)
+	ch <- prometheus.MustNewConstMetric(
+		bitswapActiveFarEndDesc, prometheus.GaugeValue, optionToFloat64(status.BitswapFarEnd),
+	)
+	ch <- prometheus.MustNewConstMetric(
+		reTxActiveNearEndDesc, prometheus.GaugeValue, optionToFloat64(status.ReTxNearEnd),
+	)
+	ch <- prometheus.MustNewConstMetric(
+		reTxActiveFarEndDesc, prometheus.GaugeValue, optionToFloat64(status.ReTxFarEnd),
+	)
+	ch <- prometheus.MustNewConstMetric(
+		attenuationNearEndDesc, prometheus.GaugeValue, status.AttenuationNearEnd,
+	)
+	ch <- prometheus.MustNewConstMetric(
+		attenuationFarEndDesc, prometheus.GaugeValue, status.AttenuationFarEnd,
+	)
+	ch <- prometheus.MustNewConstMetric(
+		crcCountNearEndDesc, prometheus.CounterValue, float64(status.CrcNearEnd),
+	)
+	ch <- prometheus.MustNewConstMetric(
+		crcCountFarEndDesc, prometheus.CounterValue, float64(status.CrcFarEnd),
+	)
+	ch <- prometheus.MustNewConstMetric(
+		erroredSecondsNearEndDesc, prometheus.CounterValue, float64(status.EsNearEnd),
+	)
+	ch <- prometheus.MustNewConstMetric(
+		erroredSecondsFarEndDesc, prometheus.CounterValue, float64(status.EsFarEnd),
+	)
+	ch <- prometheus.MustNewConstMetric(
+		severelyErroredSecondsNearEndDesc, prometheus.CounterValue, float64(status.SesNearEnd),
+	)
+	ch <- prometheus.MustNewConstMetric(
+		severelyErroredSecondsFarEndDesc, prometheus.CounterValue, float64(status.SesFarEnd),
+	)
+	ch <- prometheus.MustNewConstMetric(
+		unavailableSecondsNearEndDesc, prometheus.CounterValue, float64(status.UasNearEnd),
+	)
+	ch <- prometheus.MustNewConstMetric(
+		unavailableSecondsFarEndDesc, prometheus.CounterValue, float64(status.UasFarEnd),
+	)
+	ch <- prometheus.MustNewConstMetric(
+		hecErrorCountNearEndDesc, prometheus.CounterValue, float64(status.HecErrorsNearEnd),
+	)
+	ch <- prometheus.MustNewConstMetric(
+		hecErrorCountFarEndDesc, prometheus.CounterValue, float64(status.HecErrorsFarEnd),
+	)
+	ch <- prometheus.MustNewConstMetric(
+		losFailureCountNearEndDesc, prometheus.CounterValue, float64(status.LosFailureNearEnd),
+	)
+	ch <- prometheus.MustNewConstMetric(
+		losFailureCountFarEndDesc, prometheus.CounterValue, float64(status.LosFailureFarEnd),
+	)
+	ch <- prometheus.MustNewConstMetric(
+		lofFailureCountNearEndDesc, prometheus.CounterValue, float64(status.LofFailureNearEnd),
+	)
+	ch <- prometheus.MustNewConstMetric(
+		lofFailureCountFarEndDesc, prometheus.CounterValue, float64(status.LofFailureFarEnd),
+	)
+	ch <- prometheus.MustNewConstMetric(
+		lprFailureCountNearEndDesc, prometheus.CounterValue, float64(status.LprFailureNearEnd),
+	)
+	ch <- prometheus.MustNewConstMetric(
+		lprFailureCountFarEndDesc, prometheus.CounterValue, float64(status.LprFailureFarEnd),
+	)
+	ch <- prometheus.MustNewConstMetric(
+		lcdFailureCountNearEndDesc, prometheus.CounterValue, float64(status.LcdFailureNearEnd),
+	)
+	ch <- prometheus.MustNewConstMetric(
+		lcdFailureCountFarEndDesc, prometheus.CounterValue, float64(status.LcdFailureFarEnd),
+	)
+	ch <- prometheus.MustNewConstMetric(
+		rfecCountNearEndDesc, prometheus.CounterValue, float64(status.RfecNearEnd),
+	)
+	ch <- prometheus.MustNewConstMetric(
+		rfecCountFarEndDesc, prometheus.CounterValue, float64(status.RfecFarEnd),
+	)
+}
+
+func optionToFloat64(option bool) float64 {
+	if option {
+		return 1
+	}
+	return 0
 }
--- vigor_v5/status.go
+++ vigor_v5/status.go
@@ -35,12 +35,43 @@ type Status struct {
 	Annex      string
 	DSLVersion string
 
-	ActualRateDownstream     int
-	ActualRateUpstream       int
-	AttainableRateDownstream int
-	AttainableRateUpstream   int
-	SNRMarginDownstream      float64
-	SNRMarginUpstream        float64
+	ActualRateDownstream      int
+	ActualRateUpstream        int
+	AttainableRateDownstream  int
+	AttainableRateUpstream    int
+	InterleaveDepthDownstream int
+	InterleaveDepthUpstream   int
+	ActualPSDDownstream       float64
+	ActualPSDUpstream         float64
+	SNRMarginDownstream       float64
+	SNRMarginUpstream         float64
+
+	BitswapNearEnd     bool
+	BitswapFarEnd      bool
+	ReTxNearEnd        bool
+	ReTxFarEnd         bool
+	AttenuationNearEnd float64
+	AttenuationFarEnd  float64
+	CrcNearEnd         int
+	CrcFarEnd          int
+	EsNearEnd          int
+	EsFarEnd           int
+	SesNearEnd         int
+	SesFarEnd          int
+	UasNearEnd         int
+	UasFarEnd          int
+	HecErrorsNearEnd   int
+	HecErrorsFarEnd    int
+	LosFailureNearEnd  int
+	LosFailureFarEnd   int
+	LofFailureNearEnd  int
+	LofFailureFarEnd   int
+	LprFailureNearEnd  int
+	LprFailureFarEnd   int
+	LcdFailureNearEnd  int
+	LcdFailureFarEnd   int
+	RfecNearEnd        int
+	RfecFarEnd         int
 }
 
 func (v *Vigor) FetchStatus() (Status, error) {
@@ -85,13 +116,91 @@ func (v *Vigor) parseDSLStatusGeneralJSON(respJSON string) (Status, error) {
 		case "Attainable Rate":
 			status.AttainableRateDownstream = parseKbps(v.Get("Downstream").String())
 			status.AttainableRateUpstream = parseKbps(v.Get("Upstream").String())
+		case "Interleave Depth":
+			status.InterleaveDepthDownstream = parseKbps(v.Get("Downstream").String())
+			status.InterleaveDepthUpstream = parseKbps(v.Get("Upstream").String())
+		case "Actual PSD":
+			status.ActualPSDDownstream = parsedB(v.Get("Downstream").String())
+			status.ActualPSDUpstream = parsedB(v.Get("Upstream").String())
 		case "SNR Margin":
 			status.SNRMarginDownstream = parsedB(v.Get("Downstream").String())
 			status.SNRMarginUpstream = parsedB(v.Get("Upstream").String())
 		}
 	}
 
+	endTable := value.Get("End_Table").Array()
+	for _, v := range endTable {
+		switch v.Get("Name").String() {
+		case "Bitswap":
+			status.BitswapNearEnd = parseOption(v.Get("Near_End").String())
+			status.BitswapFarEnd = parseOption(v.Get("Far_End").String())
+		case "ReTx":
+			status.ReTxNearEnd = parseOption(v.Get("Near_End").String())
+			status.ReTxFarEnd = parseOption(v.Get("Far_End").String())
+		case "Attenuation":
+			status.AttenuationNearEnd = parsedB(v.Get("Near_End").String())
+			status.AttenuationFarEnd = parsedB(v.Get("Far_End").String())
+		case "CRC":
+			status.CrcNearEnd = parseCount(v.Get("Near_End").String())
+			status.CrcFarEnd = parseCount(v.Get("Far_End").String())
+		case "ES":
+			status.EsNearEnd = parseSeconds(v.Get("Near_End").String())
+			status.EsFarEnd = parseSeconds(v.Get("Far_End").String())
+		case "SES":
+			status.SesNearEnd = parseSeconds(v.Get("Near_End").String())
+			status.SesFarEnd = parseSeconds(v.Get("Far_End").String())
+		case "UAS":
+			status.UasNearEnd = parseSeconds(v.Get("Near_End").String())
+			status.UasFarEnd = parseSeconds(v.Get("Far_End").String())
+		case "HEC Errors":
+			status.HecErrorsNearEnd = parseCount(v.Get("Near_End").String())
+			status.HecErrorsFarEnd = parseCount(v.Get("Far_End").String())
+		case "LOS Failure":
+			status.LosFailureNearEnd = parseCount(v.Get("Near_End").String())
+			status.LosFailureFarEnd = parseCount(v.Get("Far_End").String())
+		case "LOF Failure":
+			status.LofFailureNearEnd = parseCount(v.Get("Near_End").String())
+			status.LofFailureFarEnd = parseCount(v.Get("Far_End").String())
+		case "LPR Failure":
+			status.LprFailureNearEnd = parseCount(v.Get("Near_End").String())
+			status.LprFailureFarEnd = parseCount(v.Get("Far_End").String())
+		case "LCD Failure":
+			status.LcdFailureNearEnd = parseCount(v.Get("Near_End").String())
+			status.LcdFailureFarEnd = parseCount(v.Get("Far_End").String())
+		case "RFEC":
+			status.RfecNearEnd = parseCount(v.Get("Near_End").String())
+			status.RfecFarEnd = parseCount(v.Get("Far_End").String())
+		}
+	}
+
 	return status, nil
+}
+
+func parseCount(s string) int {
+	count, err := strconv.Atoi(s)
+	if err != nil {
+		return 0
+	}
+	return count
+}
+
+func parseOption(s string) bool {
+	return s == "ON"
+}
+
+func parseSeconds(s string) int {
+	parts := strings.Fields(s)
+	if len(parts) != 2 {
+		return 0
+	}
+	if parts[1] != "s" {
+		return 0
+	}
+	x, err := strconv.Atoi(parts[0])
+	if err != nil {
+		return 0
+	}
+	return x
 }
 
 func parseKbps(s string) int {
